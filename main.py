81fc198aa8656a95473ef9ce3bbf588c8345249c
husein panjwani <husein_panjwani@hotmail.com>
2025-10-26 14:38:25 +0530

    add while loop to make the app functional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print("\nwelcome to youtube manager | choose a option ")
-print("1) list all youtube videos")
-print("2) add a youtube video")
-print("3) update a youtube video")
-print("4) delete a video")
-print("5) exit the app")
+
+while True:
+    print("\nwelcome to youtube manager | choose a option ")
+    print("1) list all youtube videos")
+    print("2) add a youtube video")
+    print("3) update a youtube video")
+    print("4) delete a video")
+    print("5) exit the app")
